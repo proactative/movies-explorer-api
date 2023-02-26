14cee394c48eb140b8ddffd6b1413f29d07f59e9
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2023-02-26 16:18:36 +0300

    refactor: config.js

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,9 @@
-const {
-  PORT = 3000,
-  PATH_MONGO = 'mongodb://127.0.0.1:27017/bitfilmsdb',
-  JWT_SECRET = 'development-key',
-} = process.env;
+const PORT = 3000;
+const PATH_MONGO = 'mongodb://127.0.0.1:27017/bitfilmsdb';
+
+let { JWT_SECRET } = process.env;
+if (process.env.NODE_ENV !== 'production') {
+  JWT_SECRET = 'development-key';
+}
 
 module.exports = { PORT, PATH_MONGO, JWT_SECRET };
